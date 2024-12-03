5f6088dd6e13a06afebba3993cfc4f411313d63d
TheRealDan <thedanielhailwood@gmail.com>
2024-12-03 23:08:42 +1300

    add day03 part2 solution

--- src/dev/therealdan/adventofcode2024/days/Day03.java
+++ src/dev/therealdan/adventofcode2024/days/Day03.java
@@ -10,7 +10,8 @@ public class Day03 {
     public Day03(String path) throws FileNotFoundException {
         File file = new File(path);
         int part1 = part1(file);
-        System.out.println("Day 3: Part 1: " + part1);
+        int part2 = part2(file);
+        System.out.println("Day 3: Part 1: " + part1 + " Part 2: " + part2);
     }
 
     public int part1(File file) throws FileNotFoundException {
@@ -28,6 +29,30 @@ public class Day03 {
                     } catch (NumberFormatException e) {
                     }
                 }
+            }
+        }
+
+        return results;
+    }
+
+    public int part2(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+
+        int results = 0;
+        boolean mulInstructionsEnabled = true;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            for (String a : line.split("mul\\(")) {
+                if (a.contains(")") && mulInstructionsEnabled) {
+                    String b = a.endsWith(")") && Arrays.stream(a.split("")).filter(c -> c.equals(")")).count() == 1 ? a.substring(0, a.length()-1) : a.split("\\)")[0];
+                    try {
+                        int result = Integer.parseInt(b.split(",")[0]) * Integer.parseInt(b.split(",")[1]);
+                        results += result;
+                    } catch (NumberFormatException e) {
+                    }
+                }
+                if (a.contains("do()")) mulInstructionsEnabled = true;
+                if (a.contains("don't()")) mulInstructionsEnabled = false;
             }
         }
 
