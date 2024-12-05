fea4cb12982f1245a649ab254dfbca1b08e90430
TheRealDan <thedanielhailwood@gmail.com>
2024-12-05 20:45:21 +1300

    add day05 part2 solution

--- src/dev/therealdan/adventofcode2024/days/Day05.java
+++ src/dev/therealdan/adventofcode2024/days/Day05.java
@@ -12,7 +12,8 @@ public class Day05 {
     public Day05(String path) throws FileNotFoundException {
         File file = new File(path);
         int part1 = part1(file);
-        System.out.println("Day 5: Part 1: " + part1);
+        int part2 = part2(file);
+        System.out.println("Day 5: Part 1: " + part1 + " Part 2: " + part2);
     }
 
     public int part1(File file) throws FileNotFoundException {
@@ -41,6 +42,48 @@ public class Day05 {
         }
 
         return result;
+    }
+
+    public int part2(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+
+        List<Rule> rules = new ArrayList<>();
+        int result = 0;
+        nextUpdate:
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            if (line.contains("|")) {
+                rules.add(new Rule(line.split("\\|")[0], line.split("\\|")[1]));
+            } else if (line.contains(",")) {
+                List<String> update = new ArrayList<>(Arrays.asList(line.split(",")));
+
+                for (Rule rule : rules) {
+                    if (line.contains(rule.first) && line.contains(rule.second)) {
+                        if (line.indexOf(rule.first) > line.indexOf(rule.second)) {
+                            sort(update, rules);
+                            result += Integer.parseInt(update.get(update.size() / 2));
+                            continue nextUpdate;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public List<String> sort(List<String> update, List<Rule> rules) {
+        update.sort((a, b) -> {
+            for (Rule sRule : rules) {
+                if (sRule.first.equals(a) && sRule.second.equals(b)) {
+                    return 1;
+                } else if (sRule.first.equals(b) && sRule.second.equals(a)) {
+                    return -1;
+                }
+            }
+            return 0;
+        });
+        return update;
     }
 
     public class Rule {
