889a6a39872bce8070e3d41f829a5b0f8ce6c5c6
TheRealDan <thedanielhailwood@gmail.com>
2024-12-10 18:43:02 +1300

    add day 10 part 2 solution

--- src/dev/therealdan/adventofcode2024/days/Day10.java
+++ src/dev/therealdan/adventofcode2024/days/Day10.java
@@ -11,7 +11,8 @@ public class Day10 {
     public Day10(String path) throws FileNotFoundException {
         File file = new File(path);
         int part1 = part1(file);
-        System.out.println("Day 10: Part 1: " + part1);
+        int part2 = part2(file);
+        System.out.println("Day 10: Part 1: " + part1 + " Part 2: " + part2);
     }
 
     public int part1(File file) throws FileNotFoundException {
@@ -32,14 +33,39 @@ public class Day10 {
         int totalTrailheads = 0;
         for (Position position : positions) {
             if (position.height != 0) continue;
-            List<Position> trailheads = search(positions, position);
+            List<Position> trailheads = searchTrailheads(positions, position);
             totalTrailheads += trailheads.size();
         }
 
         return totalTrailheads;
     }
 
-    public List<Position> search(List<Position> positions, Position currentPosition) {
+    public int part2(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+
+        List<Position> positions = new ArrayList<>();
+        int y = 0;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            int x = 0;
+            for (String number : line.split("")) {
+                positions.add(new Position(x, y, Integer.parseInt(number)));
+                x++;
+            }
+            y++;
+        }
+
+        int totalTrailheadRating = 0;
+        for (Position position : positions) {
+            if (position.height != 0) continue;
+            List<Position> trailheads = searchRatings(positions, position);
+            totalTrailheadRating += trailheads.size();
+        }
+
+        return totalTrailheadRating;
+    }
+
+    public List<Position> searchTrailheads(List<Position> positions, Position currentPosition) {
         List<Position> trailheads = new ArrayList<>();
         for (Direction direction : Direction.values()) {
             Position nextPosition = currentPosition.getPosition(positions, direction);
@@ -48,11 +74,27 @@ public class Day10 {
                 if (nextPosition.height == 9) {
                     trailheads.add(nextPosition);
                 } else {
-                    for (Position position : search(positions, nextPosition)) {
+                    for (Position position : searchTrailheads(positions, nextPosition)) {
                         if (trailheads.stream().noneMatch(trailhead -> trailhead.x == position.x && trailhead.y == position.y)) {
                             trailheads.add(position);
                         }
                     }
+                }
+            }
+        }
+        return trailheads;
+    }
+
+    public List<Position> searchRatings(List<Position> positions, Position currentPosition) {
+        List<Position> trailheads = new ArrayList<>();
+        for (Direction direction : Direction.values()) {
+            Position nextPosition = currentPosition.getPosition(positions, direction);
+            if (nextPosition == null) continue;
+            if (currentPosition.height + 1 == nextPosition.height) {
+                if (nextPosition.height == 9) {
+                    trailheads.add(nextPosition);
+                } else {
+                    trailheads.addAll(searchRatings(positions, nextPosition));
                 }
             }
         }
