9c9e763c429217252427e7b35cda132a2aa1fd1b
TheRealDan <thedanielhailwood@gmail.com>
2024-12-10 18:33:50 +1300

    add day 10 part 1 solution

--- src/dev/therealdan/adventofcode2024/AdventOfCode.java
+++ src/dev/therealdan/adventofcode2024/AdventOfCode.java
@@ -1,12 +1,12 @@
 package dev.therealdan.adventofcode2024;
 
-import dev.therealdan.adventofcode2024.days.Day09;
+import dev.therealdan.adventofcode2024.days.Day10;
 
 import java.io.IOException;
 
 public class AdventOfCode {
 
     public static void main(String[] args) throws IOException {
-        new Day09("data/day09.txt");
+        new Day10("data/day10.txt");
     }
 }
---
+++ src/dev/therealdan/adventofcode2024/days/Day10.java
@@ -0,0 +1,86 @@
+package dev.therealdan.adventofcode2024.days;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day10 {
+
+    public Day10(String path) throws FileNotFoundException {
+        File file = new File(path);
+        int part1 = part1(file);
+        System.out.println("Day 10: Part 1: " + part1);
+    }
+
+    public int part1(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+
+        List<Position> positions = new ArrayList<>();
+        int y = 0;
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            int x = 0;
+            for (String number : line.split("")) {
+                positions.add(new Position(x, y, Integer.parseInt(number)));
+                x++;
+            }
+            y++;
+        }
+
+        int totalTrailheads = 0;
+        for (Position position : positions) {
+            if (position.height != 0) continue;
+            List<Position> trailheads = search(positions, position);
+            totalTrailheads += trailheads.size();
+        }
+
+        return totalTrailheads;
+    }
+
+    public List<Position> search(List<Position> positions, Position currentPosition) {
+        List<Position> trailheads = new ArrayList<>();
+        for (Direction direction : Direction.values()) {
+            Position nextPosition = currentPosition.getPosition(positions, direction);
+            if (nextPosition == null) continue;
+            if (currentPosition.height + 1 == nextPosition.height) {
+                if (nextPosition.height == 9) {
+                    trailheads.add(nextPosition);
+                } else {
+                    for (Position position : search(positions, nextPosition)) {
+                        if (trailheads.stream().noneMatch(trailhead -> trailhead.x == position.x && trailhead.y == position.y)) {
+                            trailheads.add(position);
+                        }
+                    }
+                }
+            }
+        }
+        return trailheads;
+    }
+
+    public class Position {
+        public int x;
+        public int y;
+        public int height;
+
+        public Position(int x, int y, int height) {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+        }
+
+        public Position getPosition(List<Position> positions, Direction direction) {
+            int x = this.x + (direction.equals(Direction.LEFT) ? -1 : direction.equals(Direction.RIGHT) ? 1 : 0);
+            int y = this.y + (direction.equals(Direction.UP) ? -1 : direction.equals(Direction.DOWN) ? 1 : 0);
+            for (Position position : positions)
+                if (position.x == x && position.y == y)
+                    return position;
+            return null;
+        }
+    }
+
+    public enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+}
